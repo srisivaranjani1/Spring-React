b2bde0dce132b9c3bf3faae351559798b357d291
srisivaranjani1 <srisivaranjani.vr2023ece@sece.ac.in>
2025-07-14 20:16:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Login from './components/Login'; 
+//import Register from './components/Register';
+
+
+function App() {
+  return(
+    <div>
+      <Login />
+    </div>
+  )
+}
+
+
+export default App
